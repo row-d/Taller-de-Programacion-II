5bda8cc711be8c8341b82bf8af4c61b4cad51fcf
David Gomez <67220312+row-d@users.noreply.github.com>
2022-12-14 16:00:38 +0000

    problema p386 resuelto

---
+++ competitive/14-12-2022/busqueda-completa/p386/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+#ifndef ONLINE_JUDGE
+std::ifstream cin("input.txt");
+std::ofstream cout("output.txt");
+#endif
+
+using std::endl;
+
+int main()
+{
+    int n = 3;
+    for (size_t a = 2; a < 201; a++)
+    {
+        int a3 = a * a * a;
+        for (size_t b = 2; b < 201; b++)
+        {
+            for (size_t c = 2; c < 201; c++)
+            {
+                for (size_t d = 2; d < 201; d++)
+                {
+                    int b3 = b * b * b;
+                    int c3 = c * c * c;
+                    int d3 = d * d * d;
+                    int sum = (b3 + c3 + d3);
+                    if (sum == a3 && b < c && c < d)
+                    {
+                        cout << "Cube = " << a << ", Triple = (" << b << "," << c << "," << d << ")" << endl;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
