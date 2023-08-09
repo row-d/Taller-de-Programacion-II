1a94f1fdc2d9370236353aabaa65d381996635ee
David Gomez <davidgomez2924@gmail.com>
2023-08-09 15:26:31 +0000

    feat: add bash script to create a template copy

---
+++ competitive/create-template.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp -R "./template" "$(date +'%d-%m-%Y')"
--- competitive/template/solution.cpp
+++ competitive/template/solution.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
-
-int main()
-{
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
+#define ONLINE_JUDGE \
+    freopen("input.txt", "r", stdin);\
     freopen("output.txt", "w", stdout);
 #endif
+int main()
+{
+    ONLINE_JUDGE
     return 0;
 }
